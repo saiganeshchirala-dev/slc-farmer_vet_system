5abd742e6d5f32a51d91ebc489d5217366ed1a45
saiganeshchirala-dev <csganesh1432@gmail.com>
2026-02-05 11:58:00 +0530

    Fix CORS allowed origins for local debugging

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -37,9 +37,10 @@ class Settings(BaseSettings):
         "http://localhost:8000",
         "http://127.0.0.1",
         "http://127.0.0.1:8000",
+        "http://localhost:5500",
+        "http://127.0.0.1:5500",
         "https://slcvet.com",
-        "http://slcvet.com",
-        "*" 
+        "https://www.slcvet.com"
     ]
 
 settings = Settings()
